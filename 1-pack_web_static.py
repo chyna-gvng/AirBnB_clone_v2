c8a783b3d85895a6defad8b4f4d11604205af492
Walter <w.angoye@alustudent.com>
2023-03-23 21:50:15 +0300

    airbnb_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Comment"""
+"""
+Generates a .tgz archive from the contents
+of the web_static folder of the AirBnB Clone repo
+"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """Comment again"""
-    local("mkdir -p versions")
-
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(timestamp)
-
-    result = local("tar -cvzf {} web_static".format(archive_path))
-
-    if result.failed:
+    """ Function that makes a packs """
+    try:
+        now = datetime.now()
+        date_create = now.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        do_tgz = "web_static_{}.tgz".format(date_create)
+        local("tar -cvzf versions/{} web_static".format(do_tgz))
+        return do_tgz
+    except:
         return None
-    else:
-        return archive_path
