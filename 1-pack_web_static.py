5145baa4669ba1a4c4b0ea5650c7a2ee4275c476
Walter <w.angoye@alustudent.com>
2023-03-23 23:09:48 +0300

    airbnb_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Comment"""
+"""Compress web static package
+"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """Comment again"""
-    local("mkdir -p versions")
+    """Function to compress directory
 
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+    Return: path to archive on success; None on fail
+    """
+    # Get current time
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
 
-    result = local("tar -cvzf {} web_static".format(archive_path))
+    # Create archive
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
 
-    if result.failed:
-        return None
-    else:
+    # Check if archiving was successful
+    if result.succeeded:
         return archive_path
+    return None
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
-
-"""Starts a Flask web application"""
+"""Start web application with two routings
+"""
 
 from models import storage
 from models.state import State
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def state_list():
-    """Comment"""
-    states = storage.all('State').values()
-    return render_template(
-        "9-states.html",
-        states=states,
-        condition="states_list")
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_by_id(id):
-    """Comment"""
-    all_states = storage.all('State')
-    key = "State.{}".format(id)
-    try:
-        state = all_states[key]
-        return render_template(
-            '9-states.html',
-            state=state,
-            condition="state_id")
-    except:
-        return render_template('9-states.html', condition="not_found")
+@app.route('/states')
+@app.route('/states/<id>')
+def states_list(id=None):
+    """Render template with states
+    """
+    path = '9-states.html'
+    states = storage.all(State)
+    return render_template(path, states=states, id=id)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
