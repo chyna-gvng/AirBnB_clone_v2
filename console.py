b7d3fe208998994d7ef0d346af8988411c445c8f
Walter <w.angoye@alustudent.com>
2023-03-23 22:35:08 +0300

    airbnb_clone_v2

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the console for AirBnB"""
 import cmd
+import models
 from models import storage
 from datetime import datetime
 from models.base_model import BaseModel
