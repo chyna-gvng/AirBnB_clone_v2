05c63004923ed82b42e64a73424fb78bd693ec85
Walter <w.angoye@alustudent.com>
2023-04-05 10:42:14 +0300

    deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,16 +12,13 @@ def do_pack():
         Return: Success - '.tgz' archive path
                 Failure - None
     """
-    # Get current time
     now = datetime.now()
     now = now.strftime('%Y%m%d%H%M%S')
     archive_path = 'versions/web_static_' + now + '.tgz'
 
-    # Create archive
     local('mkdir -p versions/')
     result = local('tar -cvzf {} web_static/'.format(archive_path))
 
-    # Check if archiving was successful
     if result.succeeded:
         return archive_path
     return None
