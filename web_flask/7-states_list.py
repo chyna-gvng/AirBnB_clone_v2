f2dd5618734264d27592ef6764aa015a9d80f1d2
Walter <w.angoye@alustudent.com>
2023-04-21 07:09:22 +0300

    airbnb_clone_v2 web_flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,11 +5,14 @@
 
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from os import getenv
+
+
 app = Flask(__name__)
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     """
         Return: HTML page with list of states
@@ -32,5 +35,6 @@ def app_teardown(arg=None):
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
