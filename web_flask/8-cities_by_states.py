65517ed4229f3e19f26b9f7b0a36876a94b13ab7
Walter <w.angoye@alustudent.com>
2023-04-21 07:28:28 +0300

    airbnb_clone_v2 web_flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-"""
-    python script that starts a Flask web application
-"""
+"""Flask web application"""
 
 from models import storage
 from models.state import State
+from models.city import City
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """
-        Return: HTML page with list of states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-    return render_template(path, states=states)
-
-
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """
-        Clean-up session
-    """
+def close_session(exception=None):
+    """Close the session after every request"""
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with all the cities by states"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda x: x.name)
+
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
